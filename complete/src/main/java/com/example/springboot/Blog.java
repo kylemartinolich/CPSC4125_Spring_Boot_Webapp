25707c468756129c84aafcb76353b6d8589f821a
kylemartinolich <kylemartinolich@gmail.com>
2020-09-15 20:30:42 +0000

    Added Blog page

---
+++ complete/src/main/java/com/example/springboot/Blog.java
@@ -0,0 +1,14 @@
+package com.example.springboot;
+
+public class Blog {
+    private String input;
+
+    public String getInput(){
+        return input;
+    }
+
+    public void setEmail(String newInput){
+        this.input = newInput;
+    }
+
+}
--- complete/src/main/java/com/example/springboot/HelloController.java
+++ complete/src/main/java/com/example/springboot/HelloController.java
@@ -1,30 +1,53 @@
+  
 package com.example.springboot;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.ui.Model;
 
 @Controller
 public class HelloController {
 
-	// @GetMapping("/")
-	// public String index() {
-	// 	return "Greetings from Spring Boot!";
-    // }
-    
     @GetMapping("/")
 	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
 		model.addAttribute("name", name);
 		return "greeting";
     }
+    
     @GetMapping("/contact")
     public String contact(Model model){
         return "contact";
     }
+
+    @PostMapping("/contact")
+    public String postContact(Model model, @RequestBody Contact contact){
+        //Contact create object
+        System.out.println(contact.getEmail());
+        System.out.println(contact.getMessage());
+        return "contact";
+    }
+
     @GetMapping("/about")
     public String about(Model model){
         return "about";
+    }
+
+    //adding another GET and POST similar to contact, public postBlog(Blog)
+    //new object blog needs a string
+        @GetMapping("/blog")
+    public String blog(Model model){
+        return "blog";
+    }
+
+    @PostMapping("/blog")
+    public String postBlog(Model model, @RequestBody Blog blog){
+        //Contact create object
+        System.out.println(blog.getInput());
+
+        return "blog";
     }
 
 }
